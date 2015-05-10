62dfd27ce1df953a37ee8a0afd9ca868c6c03885
soak0205 <soak0205@gmail.com>
2015-05-09 23:42:27 -0400

    initial version

---
+++ Plot1.R
@@ -0,0 +1,35 @@
+##------------------------------------------------------------------------------------------------------##
+library(sqldf)
+##------------------------------------------------------------------------------------------------------##
+# Download the zip file and get the files in the required directory
+##------------------------------------------------------------------------------------------------------##
+
+if(!(file.exists("household_power_consumption.txt"))) {
+  fileUrl <- "http://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+  if (!file.exists("download")) {
+    dir.create("download")
+  }
+  download.file(fileUrl, destfile="download/pconsumption.zip", method="curl")
+  unzip("download/pconsumption.zip", exdir="./")
+}
+
+#read the data in specific to the dates in the assignment
+df <- read.csv2.sql(file="household_power_consumption.txt", 
+                    sql = "select * from file where Date in('1/2/2007', '2/2/2007')",
+                    sep=';', header =TRUE,row.names=NULL, na.strings='?')
+closeAllConnections()
+
+df$Date <- dmy(df$Date)
+df$Time <- hms(df$Time)
+df$date.time <- df$Date + df$Time
+df$Global_active_power <- as.numeric(df$Global_active_power)
+#-------------------------------------------------------------------------------#
+# open png device with the right file name
+png(filename='plot1.png')
+
+# Plot the data
+hist(df$Global_active_power, main='Global Active Power', xlab='Global Active Power (kilowatts)', col='red')
+
+# turn off
+dev.off()
+#-------------------------------------------------------------------------------#
